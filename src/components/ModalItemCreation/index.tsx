97bbfa93919bb0e5dc532a46bfbd2084f0a2c64f
Istiniel <eclipsio@mail.ru>
2023-04-06 15:12:18 +0300

    feat: add react-hook-form admin form

--- src/components/ModalItemCreation/index.tsx
+++ src/components/ModalItemCreation/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import useRemoveDisplayShift from '../../hooks/useRemoveDisplayShift';
 import Button from '../Button';
 import st from './ModalItemCreation.module.scss';
@@ -9,11 +9,24 @@ import { addItem } from '../../redux/features/goodsList/goodsSlice';
 import { GoodsType } from '../../API/API';
 import { checkURL } from '../../helpers/checkIsUrlToImg';
 import randomNumber from './../../helpers/randomNumber';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 type ModalItemCreationProps = {
   closeModal: () => void;
   clearForm: () => void;
   originProduct?: GoodsType;
+};
+
+type FormValues = {
+  image: string;
+  title: string;
+  measure_type: 'volume' | 'weight';
+  measure_value: string;
+  barcode: number;
+  producer: string;
+  brand: string;
+  description: string;
+  price: number;
 };
 
 const ModalItemCreation: React.FC<ModalItemCreationProps> = ({
@@ -25,168 +38,176 @@ const ModalItemCreation: React.FC<ModalItemCreationProps> = ({
 
   const dispatch = useAppDispatch();
 
-  const image = useRef<HTMLInputElement | null>(null);
-  const title = useRef<HTMLInputElement | null>(null);
-  const weight = useRef<HTMLInputElement | null>(null);
-  const volume = useRef<HTMLInputElement | null>(null);
-  const measure_value = useRef<HTMLInputElement | null>(null);
-  const barcode = useRef<HTMLInputElement | null>(null);
-  const producer = useRef<HTMLInputElement | null>(null);
-  const brand = useRef<HTMLInputElement | null>(null);
-  const description = useRef<HTMLTextAreaElement | null>(null);
-  const price = useRef<HTMLInputElement | null>(null);
-
-  function submit(e: React.SyntheticEvent) {
-    const imgURL = checkURL(image.current!.value)
-      ? image.current!.value
-      : 'https://i.ibb.co/Gdb7D4F/no-image.jpg';
-
-    const newProduct: GoodsType = {
-      url: imgURL,
-      title: title.current!.value,
-      measure_type: (weight.current?.checked ? 'volume' : 'weight') as 'volume' | 'weight',
-      measure_value: +measure_value.current!.value,
-      barcode: +barcode.current!.value,
-      producer: producer.current!.value,
-      brand: brand.current!.value,
-      description: description.current!.value,
-      price: +price.current!.value,
-      tags: tags.map((tag) => tag.replace('\n', '')) as (typeof PRODUCT_TAGS)[number][],
-      id: originProduct ? originProduct.id : randomNumber(100, 5123),
-    };
-
-    dispatch(addItem(newProduct));
-    closeModal();
-    e.preventDefault();
-  }
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    clearErrors,
+    reset,
+  } = useForm<FormValues>({ mode: 'onSubmit', reValidateMode: 'onSubmit' });
 
   useEffect(() => {
     if (originProduct) {
       setTags(originProduct.tags);
     }
-
     return () => {
       clearForm();
+      clearErrors();
+      reset();
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    const imgURL = checkURL(data.image) ? data.image : 'https://i.ibb.co/Gdb7D4F/no-image.jpg';
+
+    const newProduct: GoodsType = {
+      url: imgURL,
+      title: data.title,
+      measure_type: (data.measure_type ? 'volume' : 'weight') as 'volume' | 'weight',
+      measure_value: +data.measure_value,
+      barcode: +data.barcode,
+      producer: data.producer,
+      brand: data.brand,
+      description: data.description,
+      price: +data.price,
+      tags: tags.map((tag) => tag.replace('\n', '')) as (typeof PRODUCT_TAGS)[number][],
+      id: originProduct ? originProduct.id : randomNumber(100, 5123),
+    };
+
+    dispatch(addItem(newProduct));
+    clearForm();
+    reset();
+    closeModal();
+  };
 
   useRemoveDisplayShift();
 
   return (
     <div className={st.container} onMouseDown={closeModal}>
-      <form className={st.form} onMouseDown={(e) => e.stopPropagation()} onSubmit={submit}>
+      <form
+        className={st.form}
+        onMouseDown={(e) => e.stopPropagation()}
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className={st.inputContainer}>
           <label htmlFor="thumb">Ссылка на изображение</label>
           <input
-            ref={image}
             type="text"
             className={st.input}
-            name="thumb"
-            defaultValue={originProduct && originProduct.url}
+            defaultValue={originProduct ? originProduct.url : ''}
+            {...register('image')}
           />
         </div>
         <div className={st.inputContainer}>
           <label htmlFor="title">Наименование</label>
           <input
-            ref={title}
             type="text"
-            className={st.input}
-            name="title"
-            required
+            className={`${st.input} ${errors.title ? st.invalid : ''}`}
             defaultValue={originProduct && originProduct.title}
+            {...register('title', {
+              required: 'Введите наименование',
+            })}
           />
+          <p className={st.error}>{errors.title?.message}</p>
         </div>
         <fieldset className={st.radioContainer}>
           <div className={st.inputContainer}>
             <label htmlFor="weight">Вес</label>
             <input
-              ref={weight}
               value={'test'}
               className={st.input}
               id="weight"
-              name="measure_type"
               type="radio"
               defaultChecked={originProduct && originProduct.measure_type === 'weight'}
+              {...register('measure_type', {
+                required: 'Выберите тип',
+              })}
             />
+            <p className={st.error}>{errors.measure_type?.message}</p>
           </div>
           <div className={st.inputContainer}>
             <label htmlFor="volume">Объем</label>
             <input
-              ref={volume}
               value={'test'}
               className={st.input}
               id="volume"
-              name="measure_type"
               type="radio"
               defaultChecked={originProduct && originProduct.measure_type === 'volume'}
+              {...register('measure_type')}
             />
           </div>
         </fieldset>
         <div className={st.inputContainer}>
           <label htmlFor="measure_value">Вес/объем единицы товара</label>
           <input
-            ref={measure_value}
             type="number"
-            className={st.input}
-            name="measure_value"
-            required
+            className={`${st.input} ${errors.measure_value ? st.invalid : ''}`}
             defaultValue={originProduct && +originProduct.measure_value}
+            {...register('measure_value', {
+              required: 'Введите количество',
+            })}
           />
+          <p className={st.error}>{errors.measure_value?.message}</p>
         </div>
         <div className={st.inputContainer}>
           <label htmlFor="barcode">Штрихкод</label>
           <input
-            ref={barcode}
             type="number"
-            className={st.input}
-            name="barcode"
-            required
+            className={`${st.input} ${errors.barcode ? st.invalid : ''}`}
             defaultValue={originProduct && +originProduct.barcode}
+            {...register('barcode', {
+              required: 'Введите штрихкод',
+            })}
           />
+          <p className={st.error}>{errors.barcode?.message}</p>
         </div>
         <div className={st.inputContainer}>
           <label htmlFor="producer">Производитель</label>
           <input
-            ref={producer}
             type="text"
-            className={st.input}
-            name="producer"
-            required
+            className={`${st.input} ${errors.producer ? st.invalid : ''}`}
             defaultValue={originProduct && originProduct.producer}
+            {...register('producer', {
+              required: 'Введите производителя продукта',
+            })}
           />
+          <p className={st.error}>{errors.producer?.message}</p>
         </div>
         <div className={st.inputContainer}>
           <label htmlFor="brand">Бренд</label>
           <input
-            ref={brand}
             type="text"
-            className={st.input}
-            name="brand"
-            required
+            className={`${st.input} ${errors.brand ? st.invalid : ''}`}
             defaultValue={originProduct && originProduct.brand}
+            {...register('brand', {
+              required: 'Введите название бренда',
+            })}
           />
+          <p className={st.error}>{errors.brand?.message}</p>
         </div>
         <div className={st.inputContainer}>
           <label htmlFor="description">Описание</label>
           <textarea
-            ref={description}
-            className={st.input}
-            name="description"
-            required
+            className={`${st.input} ${errors.description ? st.invalid : ''}`}
             defaultValue={originProduct && originProduct.description}
+            {...register('description', {
+              required: 'Введите описание товара',
+            })}
           />
+          <p className={st.error}>{errors.description?.message}</p>
         </div>
         <div className={st.inputContainer}>
           <label htmlFor="price">Стоимость ед.</label>
           <input
-            ref={price}
             type="number"
-            className={st.input}
-            name="price"
-            required
+            className={`${st.input} ${errors.price ? st.invalid : ''}`}
             defaultValue={originProduct && +originProduct.price}
+            {...register('price', {
+              required: 'Введите стоимость',
+            })}
           />
+          <p className={st.error}>{errors.price?.message}</p>
         </div>
         <div className={st.tagsContainer}>
           {tags.map((tag) => (
